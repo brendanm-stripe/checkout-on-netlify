818567615946eb3ac144e1dc5ff9d6667862826c
Brendan Moore <brendanm@stripe.com>
2020-04-11 04:17:10 -0400

    Try mapping build vars to functions per

--- functions/get-urls/get-urls.js
+++ functions/get-urls/get-urls.js
@@ -1,15 +1,18 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const urls = { 
+  URL: process.env.URL, 
+  DEPLOY_URL: process.env.DEPLOY_URL || 'https://www.missing.com', 
+  DEPLOY_PRIME_URL: process.env.DEPLOY_PRIME_URL || 'https://www.missing.com',
+  UNDEF: undefined,
+  CANARY: 'tweet',
+};
+console.log(urls);
 exports.handler = async (event, context) => {
+  console.log(urls);
   try {
     return {
       statusCode: 200,
-      body: JSON.stringify({ 
-        URL: process.env.URL, 
-        DEPLOY_URL: process.env.DEPLOY_URL || 'https://www.missing.com', 
-        DEPLOY_PRIME_URL: process.env.DEPLOY_PRIME_URL || 'https://www.missing.com',
-        UNDEF: undefined,
-        CANARY: 'tweet',
-      })
+      body: JSON.stringify(urls),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
